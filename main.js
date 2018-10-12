f5cee5aea1b297643ec4eeac26e19f9e7000b30b
Gabor Szekely <gabor_szekely@hotmail.com>
2018-10-12 15:38:03 -0600

    FINAL VERSION

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const words = [
-    'creator.',
-];
+
 
 // START TYPEWRITER ON LOAD
 if (window.addEventListener) {
@@ -9,59 +7,69 @@ if (window.addEventListener) {
     window.attachEvent('onload', typewriter);
 }
 
+var counter = 0;
+
 function typewriter() {
 
     // LOOP FOR EACH WORD IN ARRAY
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        const wordLength = word.length;
-        const letterArray = [];
-        const wordBox = document.querySelector('#text');
+    //for (let i = 0; i < words.length; i++) {
+    const words = [
+        'creator. ',
+        'developer. ',
+        'designer. '
+    ];
 
-        var timeArray = [500, 600, 700, 800];
-        var randTime = timeArray[Math.floor(Math.random() * timeArray.length)];
-        var deleteDelay = 10000;
-        var iterationDelay = 1000;
-    
-
-        // CREATE LETTER ARRAY
-        for (let i = 0; i < wordLength; i++) {
-            letterArray.push(word.charAt(i));
-        }
-
-        // SPELL OUT WORD FUNCTION
-        function spell(x, callback, length) {
-            setTimeout(() => {
-                wordBox.innerHTML += letterArray[wordLength - x];
-                if (--x) spell(x, callback, length);
-            }, randTime);
-
-            callback(length);
-        }
-
-        // DELETE LETTERS FROM WORD FUNCTION
-        function remove(y) {
-            setTimeout(() => {
-                const string = wordBox.textContent;
-                const newString = string.substring(0, string.length - 1);
-                wordBox.textContent = newString;
-                if (--y) remove(y);
-            }, deleteDelay);
-        }
-
-        // CALL SPELL WORD FUNCTION
-        spell(wordLength, remove, wordLength);
+    if (counter > 2) {
+        counter = 0;
     }
+    
+    const word = words[counter];
+    const wordLength = word.length;
+    const letterArray = [];
+    const wordBox = document.querySelector('#text');
+
+    var timeArray = [400, 500];
+    var spellSpeed = timeArray[Math.floor(Math.random() * timeArray.length)];
+    var deleteSpeed = 200;
+    var deleteDelay = 2000;
+    var iterationDelay = 3000;
+
+
+    // CREATE LETTER ARRAY
+    for (let i = 0; i < wordLength; i++) {
+        letterArray.push(word.charAt(i));
+    }
+
+    var finalLetter = letterArray[wordLength - 1];
+
+    // SPELL OUT WORD FUNCTION
+    function spell(x) {
+        setTimeout(() => {
+            wordBox.innerHTML += letterArray[wordLength - x];
+            if (--x) { spell(x); }
+            else {
+                setTimeout(() => {
+                    remove(wordLength);
+                }, deleteDelay);
+            }
+        }, spellSpeed);
+    }
+
+    // DELETE LETTERS FROM WORD FUNCTION
+    function remove(y) {
+        setTimeout(() => {
+            const string = wordBox.textContent;
+            const newString = string.substring(0, string.length - 1);
+            wordBox.textContent = newString;
+            if (--y) { remove(y); }
+            else {
+                setTimeout(() => {
+                    counter += 1;
+                    typewriter();
+                }, iterationDelay);
+            }
+        }, deleteSpeed);
+    }
+
+    spell(wordLength);
 }
-
-
-
-
-
-
-// setTimeout(function () {
-//     wordBox.innerHTML += letterArray[wordLength - x];
-//     if (--x) {          // If i > 0, keep going
-//         add(x);       // Call the loop again, and pass it the current value of i
-//     }
-// }, randTime);
